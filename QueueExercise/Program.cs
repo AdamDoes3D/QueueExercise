5cb7d16b8e1dd86a7ac80a1e4c70232f8d1ae5e7
Adam Rhodes <jrhodes1183@gmail.com>
2019-01-27 16:03:01 -0600

    Queue implementation

---
+++ QueueExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace QueueExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new Queue();
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+
+
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+            queue.Dequeue();
+
+            //queue.Clear();
+            //queue.Clear();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ QueueExercise/Queue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueExercise
+{
+    class Queue
+    {
+        private readonly List<object> _queueOfObjects;
+
+        public Queue()
+        {
+            _queueOfObjects = new List<object>();
+            //Console.WriteLine("New queue has been created.");
+        }
+
+        public void Enqueue(object obj)
+        {
+            if (obj == null)
+            {
+                throw new NullReferenceException();
+            }
+            _queueOfObjects.Add(obj);
+            //Console.WriteLine($"{obj} has been added to the queue.");
+            PrintQueue();
+
+        }
+
+        public void Dequeue()
+        {
+            var dequeueThisObject = _queueOfObjects[0];
+            _queueOfObjects.RemoveAt(0);
+            //Console.WriteLine($"{dequeueThisObject} has been removed from the queue.");
+            PrintQueue();
+        }
+
+        public void Clear()
+        {
+            if (_queueOfObjects.Count == 0)
+            {
+                Console.WriteLine("Cannot clear an empty queue.");
+            }
+            else
+            {
+                _queueOfObjects.Clear();
+                Console.WriteLine("Queue has been cleared");
+            }
+        }
+
+        public void PrintQueue()
+        {
+            _queueOfObjects.ForEach(x => Console.Write(x + ","));
+            Console.WriteLine();
+        }
+    }
+}
